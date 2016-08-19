3c084e4f6e192fd17364d29df020556d3bd0c216
Caitlin <rubinca@sas.upenn.edu>
2016-08-19 09:46:14 -0400

    fixing refresh issue, got rid of useless models, add category

--- models/models.js
+++ models/models.js
@@ -1,63 +1,5 @@
 var mongoose = require('mongoose');
-var findOrCreate = require('mongoose-findorcreate')
-var Handlebars = require('handlebars')
-
-var projectSchema = mongoose.Schema({
-  projectId: String,
-  accountId: String,
-  tags: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Tag'
-  }]
-})
-
-var tagSchema = mongoose.Schema({
-  name: String,
-  tagDescription: String,
-  fields: Array,
-  trackingTrigger: String,
-  custom: String,
-  rank: Number,
-  projectId: String,
-  active: Boolean,
-  hasCallback: Boolean,
-  callbacks: Array,
-  pageName: String,
-  eventName: String
-})
-
-//tags, masters, innerCallback
-tagSchema.methods.render = function(tags, masters) {
-  //data structured {tags:, masters:, innerCallback:, template:}
-  var filtered = [];
-  if (tags) {
-     filtered = tags.filter(function(item) {
-      return this.callbacks.includes(item.name)
-    }.bind(this))
-  }
-
-  var innerCallback = '';
-  for (var i = 0; i < filtered.length; i++) {
-    innerCallback += filtered[i].render(tags, masters);
-  }
-  var master = masters.filter(function(item) {
-    return this.name === item.name
-  }.bind(this))[0]
-
-
-  var handleBarsFields = {};
-
-  for (var i = 0; i < this.fields.length; i++) {
-    handleBarsFields[this.fields[i].name] = this.fields[i].value;
-  }
-
-  handleBarsFields['callback'] = innerCallback;
-
-
-  var template = Handlebars.compile(master.template);
-  return template(handleBarsFields);
-  //return snippets[this.name](this.fields, this.trackingTrigger, innerCallback);
-}
+var findOrCreate = require('mongoose-findorcreate');
 
 var masterSchema = mongoose.Schema({
   name: String,
@@ -67,18 +9,17 @@ var masterSchema = mongoose.Schema({
   hasCallback: Boolean,
   approved: Boolean,
   callbackCode: String,
-  template: String
-})
+  template: String,
+  category: String
+});
 
 var userSchema = mongoose.Schema({
   username: String,
   password: String
 });
 
-projectSchema.plugin(findOrCreate);
+masterSchema.plugin(findOrCreate);
 module.exports = {
-  'Project': mongoose.model('Project', projectSchema),
-  'Tag': mongoose.model('Tag', tagSchema),
   'Master': mongoose.model('Master', masterSchema),
   'User': mongoose.model('User', userSchema)
-  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var Master = require('../models/models').Master;
+var Utils = require('../utils');
 
 router.use(function(req, res, next) {
 	if (req.user) {
 		next();
 	}
 	else {
-		res.redirect('/login')
+		res.redirect('/login');
 	}
 });
 
 router.get('/', function(req, res, next) {
-  console.log('got here')
+  console.log('got here');
   Master.find({"approved": false}, function(err, masters) {
-    console.log("THESE ARE THE MASTERS,", masters)
-    res.render('index', {masters: masters})
-  })
-})
+    console.log("THESE ARE THE MASTERS,", masters);
+    res.render('index', {masters: masters});
+  });
+});
 
 router.post('/', function(req, res, next) {
-  var utils = require('../utils')
-  //TODO this needs to be changed to incorporate master from form
-  //find the master to UPDATE
-  //change it's template to handlebars compiled code
-  //change approved to true
+  var utils = new Utils();
+	utils.category = req.query.category;
   Master.findOne({'name': req.query.name})
         .then(utils.approve.bind(utils))
         .then(function(response) {
           console.log(response);
-          console.log("all of this worked now im redirecting")
-          //ASK: why is this not clearing when I redirect?
           res.redirect('/');
         })
         .catch(function(err) {
-          console.log("Error at the end of /options", err)
-        })
+          console.log("Error at the end of /options", err);
+        });
 });
 
 module.exports = router;
